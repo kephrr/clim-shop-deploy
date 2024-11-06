256a5279d175146193e38b0c12c537ac17e1f06a
Kephren NZE <kephrennze@gmail.com>
2024-11-06 14:49:13 +0000

    "Added more stats on the admin dashboard

--- src/main/java/soft_afric/clim/shop/clim_shop/web/dto/response/DashBoardDto.java
+++ src/main/java/soft_afric/clim/shop/clim_shop/web/dto/response/DashBoardDto.java
@@ -24,16 +24,27 @@ public class DashBoardDto {
     private int chiffreAffaireMensuel;
     private int clientsActifs;
     private int stockClims;
+    private String TauxCA;
+    private String TauxCommande;
 
     public static DashBoardDto toDto(List<Commande> commandes, List<Client> clients, List<Clim> clims) {
         int commandesEnCours = 0;
         int chiffreAffaireMensuel = 0;
+        int chiffreAffaireDernierMensuel = 0;
 
         // Créer une instance de Calendar pour obtenir le mois et l'année actuels
         Calendar calendar = Calendar.getInstance();
         int moisActuel = calendar.get(Calendar.MONTH);
         int anneeActuelle = calendar.get(Calendar.YEAR);
 
+
+        calendar.add(Calendar.MONTH, -1); // Soustraire un mois
+        // Récupérer les informations du mois précédent
+        int moisDernier = calendar.get(Calendar.MONTH) + 1; // Les mois sont indexés à partir de 0
+        int annee = calendar.get(Calendar.YEAR);
+
+        int commandesMensuelles = 0;
+        int commandesDernierMensuelles = 0;
         for (Commande c : commandes) {
             // Créer une instance de Calendar pour la date de la commande
             calendar.setTime(c.getDateCommmande());
@@ -43,10 +54,18 @@ public class DashBoardDto {
             // Vérifier si le mois et l'année de la commande correspondent au mois et à l'année actuels
             if (moisCommande == moisActuel && anneeCommande == anneeActuelle) {
                 chiffreAffaireMensuel += c.getMontantFinal();
+                commandesMensuelles += 1;
             }
+            if (moisCommande == moisDernier && anneeCommande == annee) {
+                chiffreAffaireDernierMensuel += c.getMontantFinal();
+                commandesDernierMensuelles += 1;
+            }
+
             // Commandes en cours
             if(c.getEtatCommande()== EtatCommande.Encour) commandesEnCours+=1;
         }
+
+
 
         // Stock de clims
         int stockClims = 0;
@@ -57,11 +76,25 @@ public class DashBoardDto {
         // Clients Actifs
         int clientsActifs = clients.size();
 
+        float tauxCA = (float) (chiffreAffaireMensuel - chiffreAffaireDernierMensuel) / (chiffreAffaireDernierMensuel * 100);
+
+        float tauxCommandes = (float) (commandesMensuelles - commandesDernierMensuelles) / (commandesDernierMensuelles * 100);
+
         return DashBoardDto.builder()
                 .commandesEnCours(commandesEnCours)
                 .chiffreAffaireMensuel(chiffreAffaireMensuel)
                 .clientsActifs(clientsActifs)
                 .stockClims(stockClims)
+                .TauxCommande(getInd(tauxCommandes)+tauxCA+" %")
+                .TauxCA(getInd(tauxCA)+tauxCA+" %")
                 .build();
+    }
+
+    public static String getInd(float val){
+        if(0>val) {
+            return "-";
+        }
+        return "+";
+
     }
 }
